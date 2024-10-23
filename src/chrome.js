e2310aacd60a071530951b5aa3654ab3c4703ed9
Krzysztof Modras <chrmod@chrmod.net>
2024-10-23 10:13:01 +0200

    Manual recording setup

--- src/chrome.js
+++ src/chrome.js
@@ -1,17 +1,17 @@
 import { remote } from "webdriverio";
 import { getExtensionPath } from "./extension.js";
 
-export async function getChrome() {
+export async function getChrome({ headless = true } = {}) {
   const browser = await remote({
     capabilities: {
       browserName: "chrome",
       webSocketUrl: true,
       "goog:chromeOptions": {
         args: [
-          "headless",
+          headless ? 'headless' : undefined,
           `--load-extension=${getExtensionPath()}`,
           "--disable-search-engine-choice-screen",
-        ],
+        ].filter(Boolean),
         prefs: {
           // enable "Extended preloading" for speculationrules
           "net.network_prediction_options": 3,
